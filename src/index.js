d40a67dbbcbefe96a58bd585728a7dd92ad71b8d
mashkovtsevasf <103369052+mashkovtsevasf@users.noreply.github.com>
2022-09-27 19:29:55 +0300

    work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let listOfLi = document.getElementsByTagName("li");
+    for (let i = 0; i < listOfLi.length; i++){
+        if (i % 2){
+            listOfLi[i].style.backgroundColor = "red";
+        }; 
+    }; 
+};
 
 export const findElement = () => {
     let likedLi = dociment.getElementsByClassName('likedItem');
