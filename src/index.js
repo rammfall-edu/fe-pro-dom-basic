233a09b9b5d3ff1052d9881e5843f96c9d41d1fd
mashkovtsevasf <103369052+mashkovtsevasf@users.noreply.github.com>
2022-09-27 19:25:14 +0300

    work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 export const paintCards = () => {};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let likedLi = dociment.getElementsByClassName('likedItem');
+    for ( let i = 0; i < likedLi.length; i++) {
+        likedLi[i].style.backgroundColor = "blue";
+    }
+};
